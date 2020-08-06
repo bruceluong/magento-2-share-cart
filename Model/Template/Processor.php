d15b82c864b03d5aafa962647aaf94e380dd7029
ndluong <luongnd@mageplaza.com>
2020-08-06 14:40:03 +0700

    test

--- Model/Template/Processor.php
+++ Model/Template/Processor.php
@@ -13,7 +13,7 @@
  * Do not edit or add to this file if you wish to upgrade this extension to newer
  * version in the future.
  *
- * @category    Mageplaza
+ * @category    Mageplaza1111
  * @package     Mageplaza_ShareCart
  * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
  * @license     https://www.mageplaza.com/LICENSE.txt
